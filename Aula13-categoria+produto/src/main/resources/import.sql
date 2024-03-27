7f09d90347737d9ef7fddd871fb01809f2566762
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-27 09:51:23 -0300

    aula13

---
+++ Aula13-categoria+produto/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO tb_categoria(nome) VALUES('Eletronico');
+INSERT INTO tb_categoria(nome) VALUES('Frutas');
+INSERT INTO tb_categoria(nome) VALUES('Verduras');
+INSERT INTO tb_categoria(nome) VALUES('Produto de limpeza');
+INSERT INTO tb_categoria(nome) VALUES('Carnes');
+INSERT INTO tb_produto(nome, descricao, valor,categoria_id) VALUES('Mouse Microsoft', 'Mouse sem fio', 250.0,1);
+INSERT INTO tb_produto(nome, descricao, valor,categoria_id) VALUES('Smartphone Samsung Galaxy A54 5G', 'Samsung Galaxy A54 5G', 1799.0,2);
+INSERT INTO tb_produto(nome, descricao, valor,categoria_id) VALUES('Smart TV', 'Smart TV LG LED 65 polegadas', 3999,1);
